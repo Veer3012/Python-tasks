78c3b39174deb722a95458907b1021632d75a06b
Veer3012 <veer.thakkar2005.vt@gmail.com>
2026-01-21 12:24:43 +0530

    Create task2.py

---
+++ task2.py
@@ -0,0 +1,10 @@
+##problem statement-2
+num1 = int(input("Enter a number : "))
+
+sqrt=math.sqrt(num1)
+log=math.log(num1)
+sine =math.sin(num1)
+
+print(f"Square root of {num1}: {sqrt}")
+print(f"Natural logarithm of {num1}: {log}")
+print(f"Sine of {num1}: {sine}")
