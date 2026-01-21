49286aad4872ec545ef4da96ec4dbd1a278663a4
Veer3012 <veer.thakkar2005.vt@gmail.com>
2026-01-21 12:24:04 +0530

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,13 @@
+##problem statement-1
+import math
+
+
+def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    else:
+        return n * factorial(n - 1)
+
+num = int(input("Enter a number to find its factorial: "))
+result = factorial(num)
+print(f"The factorial of {num} is: {result}")
